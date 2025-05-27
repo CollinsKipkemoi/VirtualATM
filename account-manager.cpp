bd8070726406a6bbc15940c5bb4bb0c3edea4919
Collins Kipkemoi <chemerilcollins@gmail.com>
2025-05-27 02:51:25 +0200

    Enhance account management functionality by adding deposit and withdraw operations, and update balance retrieval method. Also, modify .gitignore to exclude IDE files.

--- account-manager.cpp
+++ account-manager.cpp
@@ -4,12 +4,39 @@
 
 using namespace std;
 
-double getBalance(unordered_map<string, tuple<string, double, string>> data, string account)
+double getBalance(const unordered_map<string, tuple<string, double, string>>& data, string account)
 {
     auto userData = data.find(account);
     if (userData != data.end()) {
         double balance = get<1>(userData->second);
         return balance;
+    }
+    throw runtime_error("Account not found");
+}
+
+double deposit(unordered_map<string, tuple<string, double, string>>& data, string account, double amount)
+{
+    auto userData = data.find(account);
+    if (userData != data.end()) {
+        double balance = get<1>(userData->second);
+        balance += amount;
+        get<1>(userData->second) = balance;
+        return balance;
+    }
+    throw runtime_error("Account not found");
+}
+
+double withdraw(unordered_map<string, tuple<string, double, string>>& data, string account, double amount)
+{
+    auto userData = data.find(account);
+    if (userData != data.end()) {
+        double balance = get<1>(userData->second);
+        if (balance >= amount) {
+            balance -= amount;
+            get<1>(userData->second) = balance;
+            return balance;
+        }
+        throw runtime_error("Insufficient balance");
     }
     throw runtime_error("Account not found");
 }
